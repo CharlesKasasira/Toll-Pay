e168e86f04f637aacee98fe61ce6fbd71e9b4faa
Charles Kasasira <charleskasasira01@gmail.com>
2022-09-26 10:21:09 +0300

    created ysave project

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Supabase Flutter',
+      title: 'YSAVE',
       theme: ThemeData.dark().copyWith(
         primaryColor: Colors.green,
         textButtonTheme: TextButtonThemeData(
