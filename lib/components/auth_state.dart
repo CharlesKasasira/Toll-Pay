70d0887e6ca5927036b63309e2820c6fa5b27095
Charles Kasasira <charleskasasira01@gmail.com>
2022-10-10 15:01:36 +0300

    feat: added roles -driver, organization, operator, admin

--- lib/components/auth_state.dart
+++ lib/components/auth_state.dart
@@ -12,9 +12,19 @@ class AuthState<T extends StatefulWidget> extends SupabaseAuthState<T> {
 
   @override
   void onAuthenticated(Session session) {
+    final meta = session.user;
     if (mounted) {
-      Navigator.of(context)
+      if(meta?.userMetadata['roles'] == "operator"){
+        Navigator.of(context)
+          .pushNamedAndRemoveUntil('/operator-dashboard', (route) => false);
+      } else if (meta?.userMetadata['roles'] == "organization"){
+        Navigator.of(context)
+          .pushNamedAndRemoveUntil('/org-dashboard', (route) => false);
+      } else {
+        Navigator.of(context)
           .pushNamedAndRemoveUntil('/dashboard', (route) => false);
+      }
+      
     }
   }
 
